479273c79048312b17d11af29ddcd6dc43954fb7
Neimerc <Neil_Mercury@yahoo.es>
2020-05-19 18:57:44 +0200

    create REST API

---
+++ src/main/java/com/neimerc/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.neimerc.ecommerce.dao;
+
+import com.neimerc.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
